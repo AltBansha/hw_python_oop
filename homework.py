feee6ab4d930ce33c369eb008fdcc9abadbc8af1
Yulia <bannikovaj@gmail.com>
2020-11-19 11:22:54 +0500

    I hope it is the last corrections

--- homework.py
+++ homework.py
@@ -23,22 +23,15 @@ class Calculator:
         self.records.append(Record)
 
     def get_today_stats(self):
-        today_stats = []
-        today = dt.date.today()
-        for record in self.records:
-            if record.date == today:
-                today_stats.append(record.amount)      
-        return sum(today_stats)
+        today = dt.date.today()     
+        return sum(record.amount for record in self.records
+        if record.date == today)
 
     def get_week_stats(self):
-        week_stats = []
         today = dt.date.today()
-        week = dt.timedelta(days=7)
-        start_date = today - week
-        for record in self.records:
-            if start_date < record.date <= today:
-                week_stats.append(record.amount)
-        return sum(week_stats)
+        start_date = today - dt.timedelta(days=7)
+        return sum(record.amount for record in self.records
+        if start_date < record.date <= today)
 
 
 class CashCalculator(Calculator):
@@ -49,36 +42,39 @@ class CashCalculator(Calculator):
         'eur': ['Euro', EURO_RATE],
         'rub': ['руб', 1]
         }
+    NO_MONEY = 'Денег нет, держись'
+    REMAINED = 'На сегодня осталось'
+    DEBT = 'Денег нет, держись: твой долг -'
 
     def get_today_cash_remained(self, currency):
-        currency_title, currency_rate = self.CURRENCIES[currency]
-        today_remained = (round(((self.limit - self.get_today_stats())/currency_rate), 2))
-        if today_remained == 0:
-            return "Денег нет, держись"
-        elif today_remained > 0:
-            return f'На сегодня осталось {today_remained} {currency_title}'                 
+        title, rate = self.CURRENCIES[currency]
+        remained = round((self.limit - self.get_today_stats())/rate, 2)
+        if remained > 0:
+            return f'{self.REMAINED} {remained} {title}'
+        elif remained < 0:                         
+            return f'{self.DEBT} {abs(remained)} {title}'
+        else:
+            return self.NO_MONEY     
         
-        return f'Денег нет, держись: твой долг - {abs(today_remained)} {currency_title}' 
-
-        
+                                   
 class CaloriesCalculator(Calculator):
+    REMAINED = 'Сегодня можно съесть что-нибудь ещё, но с общей '\
+               'калорийностью не более {remained} кКал' 
+    ENOUGH = 'Хватит есть!'                
 
     def get_calories_remained(self):
         today_calories = self.get_today_stats()
         calories_remained = self.limit - today_calories
-        answer_remained = (f'Сегодня можно съесть что-нибудь ещё, ' 
-                f'но с общей калорийностью не более {calories_remained} кКал')
         if calories_remained > 0:
-            return answer_remained
-        
-        return f'Хватит есть!'
+            return self.REMAINED.format(remained=calories_remained)
+        return self.ENOUGH
       
 
 if __name__ == "__main__":
 
     Cash = CashCalculator(10000)
     Calories = CaloriesCalculator(2600)   
-    r1 = Record(amount=10000, comment="Безудержный шопинг")
+    r1 = Record(amount=7500, comment="Безудержный шопинг")
     r2 = Record(amount=6000, comment="Вино", date="14.11.2020")
     r3 = Record(amount=1000, comment="Конфеты")
     r4 = Record(amount=1000, comment="Тортик")
